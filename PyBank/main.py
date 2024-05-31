20498595b37cc2cc63fa50ef0ced185195197716
ArzooF <afyxclsvgmail.com>
2024-05-31 16:34:23 -0400

    -m module 3 challenge files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,57 @@
-print("hello_world_pybank")
+import csv 
+import os
+
+inputFile = ("./Resources/budget_data.csv")
+outputFile = ("analysis.txt")
+
+total_months = 0
+month_of_change = []
+net_change_list = []
+greatest_inc = ["",0]
+greatest_dec = ["", 9999999999]
+total_net = 0
+
+
+with open(inputFile, "r") as file:
+    reader = csv.reader(file)
+    header = next(reader)
+
+    first_row = next(reader)
+    total_months += 1
+    total_net += int(first_row[1])
+    prev_net = int(first_row[1])
+
+    for row in reader:
+        total_months+=1
+        total_net += int(row[1])
+
+        net_change = int(row[1]) - prev_net
+        prev_net = int(row[1])
+        net_change_list += [net_change]
+        month_of_change += [row[0]]
+
+        if net_change > greatest_inc[1]:
+            greatest_inc[1] = net_change
+            greatest_inc[0] = row[0]
+        if net_change < greatest_dec[1]:
+            greatest_dec[1] = net_change
+            greatest_dec[0] = row[0]
+
+net_monthly_avg = round(sum(net_change_list) / len(net_change_list),2)
+
+output = (
+    f"Financial Analysis\n"
+    f"---------------------\n"
+    f"Total Month: {total_months}\n"
+    f"Total: ${total_net}\n"
+    f"Average Change: {net_monthly_avg}\n"
+    f"Greatest Inc: {greatest_inc[0]}  (${greatest_inc[1]})\n"
+    f"Greatest dec: {greatest_dec[0]} (${greatest_dec[1]})"
+
+
+)
+
+print(output)
+
+with open(outputFile,"w") as analysis:
+    analysis.write(output)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,79 @@
 print("pypoll")
+
+
+import os
+import csv
+from pathlib import Path 
+
+# Assign file location with the pathlib library
+csv_file_path = Path("Resources", "election_data.csv")
+
+# Declare Variables 
+total_votes = 0 
+candidate_options = []
+candidate_votes = {}
+vote_percentage = 0
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+
+# Open csv in default read mode with context manager
+with open(csv_file_path, encoding="utf-8") as elections:
+
+    # Store data under the csvreader variable
+    csvreader = csv.reader(elections,delimiter=",") 
+
+    # Skip the header so we iterate through the actual values
+    header = next(csvreader)     
+
+    # Iterate through each row in the csv
+    for row in csvreader: 
+
+        # Count the unique Voter ID's and store in variable called total_votes
+        total_votes +=1
+
+        candidate_name = row[2]
+
+
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
+
+# Print a the summary of the analysis
+output_file = Path("Election_Results_Summary.txt")
+with open(output_file,"w") as analysis:
+# Print the summary table
+    election_results = (
+        f"Election Results\n"
+        f"----------------------------\n"
+        f"Voter Output: {total_votes}\n"
+        f"----------------------------\n"
+    )
+    analysis.write(election_results)
+    
+
+    print(election_results)
+
+    for candidate in candidate_votes:
+        votes = candidate_votes.get(candidate)
+        vote_percentage = float(votes)/ float(total_votes) *100
+
+        if (votes > winning_count):
+            winning_count = votes
+            winning_candidate = candidate
+            winning_percentage = vote_percentage 
+
+        voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
+        analysis.write(voter_output)
+        print(voter_output)
+    winning_summary = (
+        f"Winner : {winning_candidate}\n"
+        f"Winner percentage: {winning_percentage:.3f}%\n"
+    )
+    print(winning_summary)
+
+# write the analysis
+    analysis.write(winning_summary)
+    
